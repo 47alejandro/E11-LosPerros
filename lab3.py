652753c6da7490a4129e90c38d1ebc01ee1f747d
Juan-Cisneros <110487068+Juan-Cisneros@users.noreply.github.com>
2023-02-08 15:57:42 -0800

    Update lab3.py

--- lab3.py
+++ lab3.py
@@ -11,7 +11,7 @@ uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
 
 # Connect to a PM2.5 sensor over UART
 from adafruit_pm25.uart import PM25_UART
-pm25 = PM25_UART(uart, none)
+pm25 = PM25_UART(uart, None)
 
 # Create library object, use 'slow' 100KHz frequency!
 # i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
