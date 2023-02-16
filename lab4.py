82fd79370fe1c737410406877c607eddb7f328c4
Juan-Cisneros <110487068+Juan-Cisneros@users.noreply.github.com>
2023-02-15 16:23:51 -0800

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -19,6 +19,11 @@ itime = start_time
 starttime = time.time()
 end = starttime + 30
 
+i2c = board.I2C()
+bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
+
+bme680.sea_level_pressure = 1013.25
+
 # For use with Raspberry Pi/Linux:
 import serial
 uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25) 
