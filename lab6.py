e7c714172b2ba584ef15ed2db412dd0af09ef938
47alejandro <108770237+47alejandro@users.noreply.github.com>
2023-02-22 15:48:59 -0800

    Update lab6.py

--- lab6.py
+++ lab6.py
@@ -9,13 +9,15 @@ import sys
 from digitalio import DigitalInOut, Direction, Pull
 from adafruit_pm25.i2c import PM25_I2C
 
+time.sleep(120)
 print(sys.argv)
 
 start_time = time.time()
 run_time = int(sys.argv[1])
 itime = start_time
 
-save = datetime.datetime.now()
+n1 = datetime.now()
+save = n1.strftime("%Y-%m-%d %H:%M:%S")
 
 i2c = board.I2C()
 bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
@@ -35,7 +37,7 @@ pm25 = PM25_UART(uart, None)
 # Connect to a PM2.5 sensor over I2C
 # pm25 = PM25_I2C(i2c, reset_pin)
 print("Found PM2.5 sensor, reading data...")
-f = open("data" + str(save) + ".csv","w", newline='')
+f = open("data" + save + ".csv","w", newline='')
 writer = csv.writer(f)
 metadata = ["PM1", "PM2.5", "PM10","Tempature", "Gas", "Humidity", "Pressure", "Altitude", "TimeinSeconds", "Time"]
 writer.writerow(metadata)
