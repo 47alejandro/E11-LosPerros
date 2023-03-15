0b6be507718227b3cbffb82395934b1f7202ab3c
Juan-Cisneros <110487068+Juan-Cisneros@users.noreply.github.com>
2023-03-15 16:36:54 -0700

    Add files via upload

---
+++ lab9.py
@@ -0,0 +1,45 @@
+import datetime
+import csv
+import sys
+import argparse
+import time
+import RPi.GPIO as GPIO
+
+
+runtime = int(sys.argv[1])
+currenttime = int(time.time())
+counttime = int(time.time())
+
+#GPIO pin set up
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+#variable
+count = 0
+
+# functiondefinition 
+def count_pulse(channel):
+    global count
+    count += 1
+    print(time.time())
+
+
+GPIO.add_event_detect(26, GPIO.FALLING, callback=count_pulse)
+
+meta_data = ["Time","Count"]
+f = open("RadiationCount.csv","w",newline = '')
+writer = csv.writer(f)
+writer.writerow(meta_data)
+
+while counttime < currenttime+runtime:
+    counttime = int(time.time())
+    try:
+        count = 0
+        time.sleep(60)
+        print("CountsForCurrentMinute:", count)
+    except KeyboardInterrupt: 
+        GPIO.cleanup()
+    data = [time.time(),count]
+    writer.writerow(data)
+    
+f.close()
